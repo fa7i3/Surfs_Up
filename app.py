b06e91ff601e54cfd54abdc63cd9e396e0de1f1f
Faith Emenike <akemu_faith@yahoo.com>
2022-06-30 20:23:15 -0500

    commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #### Set Up the Flask Weather App
 # Import dependencies
 
+from cProfile import run
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -34,3 +35,19 @@ session = Session(engine)
 ##################################
 app = Flask(__name__)
 
+####################################
+# Module 9.5.2
+# Create the Welcome Route #
+####################################
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+flask run
